5117a604551574551c730a3d4dfc97934d0400e0
chanikya2006 <chanikyaganteda@gmail.com>
2026-06-10 22:44:26 +0530

    Fixed frontend cors error

--- Backend/backend_final.py
+++ Backend/backend_final.py
@@ -12,7 +12,7 @@ import os
 from supabase import create_client, Client
 
 app = Flask(__name__)
-CORS(app, origins=["http://localhost:3000", "chrome-extension://*"], supports_credentials=True)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 # ---------- Supabase Setup ----------
 SUPABASE_URL = os.environ.get("SUPABASE_URL")
